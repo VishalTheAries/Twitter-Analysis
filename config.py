3610f7572f4c7ad16df0448a3337bf16104fee20
vishal <vishalgupta013@gmail.com>
2017-07-08 22:30:09 +0530

    Config FIle

---
+++ config.py
@@ -0,0 +1,15 @@
+
+
+# Twiter API 
+consumer_key="XorHCfXwlJI6lx4bVGxgkv7Qb"
+consumer_secret="orNAnnTGxpJXTbir16yDMqNdxn7KoTShBXMW2gk14x2SN8vK7t"
+access_token="161152310-l3FOKvAvt0fqZuyV3QoWissZJFOzmhXhim0gWYtQ"
+access_token_secret="Z93qMGc6gTdUsI7u0gjzsLnDY3E1VrjgIs09LR1RVatmk"
+
+#MONGODB
+MONGO_HOST = 'mongodb://vishal:vishal@ds145892.mlab.com:45892/heroku_00mz69jn'
+PROJECT_NAME = 'twitterdata'
+
+
+#Tweet withh keywords to extract
+keywords = ['analytics']
