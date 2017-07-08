fb8577acff4ec865ab086b4d8de791c3d1207f7c
VishalTheAries <vishalgupta013@gmail.com>
2017-07-08 23:17:41 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,15 +1,13 @@
-
-
 # Twiter API 
-consumer_key="XorHCfXwlJI6lx4bVGxgkv7Qb"
-consumer_secret="orNAnnTGxpJXTbir16yDMqNdxn7KoTShBXMW2gk14x2SN8vK7t"
-access_token="161152310-l3FOKvAvt0fqZuyV3QoWissZJFOzmhXhim0gWYtQ"
-access_token_secret="Z93qMGc6gTdUsI7u0gjzsLnDY3E1VrjgIs09LR1RVatmk"
+consumer_key=""
+consumer_secret=""
+access_token=""
+access_token_secret=""
 
 #MONGODB
-MONGO_HOST = 'mongodb://vishal:vishal@ds145892.mlab.com:45892/heroku_00mz69jn'
-PROJECT_NAME = 'twitterdata'
+MONGO_HOST = ''
+PROJECT_NAME = ''
 
 
-#Tweet withh keywords to extract
+#Tweet with keywords to extract
 keywords = ['analytics']
